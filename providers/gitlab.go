6202721c94afe961826bf77887924f3f7636ce10
MARGAIL Maurin <issugodu13@gmail.com>
2025-11-13 20:03:08 +0100

    change gitlab provider

--- providers/gitlab.go
+++ providers/gitlab.go
@@ -162,7 +162,7 @@ func (p *GitLabProvider) getUserinfo(ctx context.Context, s *sessions.SessionSta
 
 	// Build user info url from login url of GitLab instance
 	userinfoURL := *p.LoginURL
-	userinfoURL.Path = "/oauth/userinfo"
+	userinfoURL.Path = *p.LoginURL.Path + "/oauth/userinfo"
 
 	var userinfo gitlabUserinfo
 	err := requests.New(userinfoURL.String()).
@@ -238,11 +238,8 @@ type gitlabProjectInfo struct {
 func (p *GitLabProvider) getProjectInfo(ctx context.Context, s *sessions.SessionState, project string) (*gitlabProjectInfo, error) {
 	var projectInfo gitlabProjectInfo
 
-	endpointURL := &url.URL{
-		Scheme: p.LoginURL.Scheme,
-		Host:   p.LoginURL.Host,
-		Path:   "/api/v4/projects/",
-	}
+	endpointURL := *p.LoginURL
+        endpointURL.Path := *p.LoginURL.Path + "/api/v4/projects/"
 
 	err := requests.New(fmt.Sprintf("%s%s", endpointURL.String(), url.QueryEscape(project))).
 		WithContext(ctx).
